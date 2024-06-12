7ba784c38baa3f3d3a8c224ff47b8f92a4caa3a0
Paulo Sergio <036paulo@gmail.com>
2024-06-11 22:11:35 -0300

    Exercicio concluido conforme as aulas da alura

---
+++ projetos_propostos/sorteador-numeros/app.js
@@ -0,0 +1,50 @@
+
+function sortear(){
+  let quantidade = document.getElementById('quantidade').value;
+  let de = parseInt( document.getElementById('de').value );
+  let ate = parseInt( document.getElementById('ate').value );
+
+  let numeroSorteado = [];
+  let numero;
+
+  for (let i=0; i<quantidade;i++){
+    numero = obterNumeroAleatorio(de,ate);
+
+    while(numeroSorteado.includes(numero)){
+      numero = obterNumeroAleatorio(de,ate);
+    }
+
+    numeroSorteado.push( numero );
+  }
+
+  let resultado = document.getElementById('resultado');
+  resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${numeroSorteado}</label>`;
+
+  alterarStatusBotao();
+}
+function obterNumeroAleatorio(min, max){
+  /**
+   * documentacao https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  **/
+  return Math.floor( Math.random() * (max - min + 1) ) + min;
+}
+function alterarStatusBotao(){
+  let botao = document.getElementById('btn-reiniciar');
+  if (botao.classList.contains('container__botao-desabilitado')){
+    botao.classList.remove('container__botao-desabilitado');
+    botao.classList.add('container__botao');
+  }else{
+    botao.classList.remove('container__botao');
+    botao.classList.add('container__botao-desabilitado'); 
+  }
+}
+
+function reiniciar(){
+
+  document.getElementById('quantidade').value = '';
+  document.getElementById('de').value = '';
+  document.getElementById('ate').value = '';
+  document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+
+  alterarStatusBotao();
+}
