10e24226bfc02ce239db1139f2423efc4c63c07c
Paulo Sergio <036paulo@gmail.com>
2024-06-11 22:34:53 -0300

    simplificando a condicional para retornar null

--- projetos_propostos/sorteador-numeros/app.js
+++ projetos_propostos/sorteador-numeros/app.js
@@ -10,24 +10,25 @@ function sortear(){
   if(de >= ate){
     alert('Valor DO NÚMERO deve ser vaior que o ATE O NÚMERO');
     reiniciar();
-  }else{
+    return
+  }
 
-    for (let i=0; i<quantidade;i++){
+  for (let i=0; i<quantidade;i++){
+    numero = obterNumeroAleatorio(de,ate);
+
+    while(numeroSorteado.includes(numero)){
       numero = obterNumeroAleatorio(de,ate);
-
-      while(numeroSorteado.includes(numero)){
-        numero = obterNumeroAleatorio(de,ate);
-      }
-
-      numeroSorteado.push( numero );
     }
 
-    let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${numeroSorteado}</label>`;
-
-    alterarStatusBotao();
+    numeroSorteado.push( numero );
   }
+
+  let resultado = document.getElementById('resultado');
+  resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${numeroSorteado}</label>`;
+
+  alterarStatusBotao();
 }
+
 function obterNumeroAleatorio(min, max){
   /**
    * documentacao https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
